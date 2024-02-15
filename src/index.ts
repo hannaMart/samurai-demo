bf26ef7c0aa410db94cf3cdabd1d9b0c69895a84
NOVA <anna_tandem@mail.ru>
2024-02-16 00:14:08 +0100

    change

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, {Request, Response} from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   let startMessage = 'Hello Hania- oops ))!'
   res.send(startMessage)
 })
 
-app.listen(port, () => {
+app.listen(port,  () => {
   console.log(`Example app listening on port ${port}`)
 })
