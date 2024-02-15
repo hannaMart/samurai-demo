46b123f79d3a81414aa1e83d5b5e167606c2cb23
NOVA <anna_tandem@mail.ru>
2024-02-16 00:17:14 +0100

    and final try!

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  let startMessage = 'Hello Hania- oops ))!'
+  let startMessage = 'Hello Hania- oopsy, Hania! you did IT ))!'
   res.send(startMessage)
 })
 
