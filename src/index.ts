f96d3a05901d8ea6f013585b2ae5bab49df2d385
NOVA <anna_tandem@mail.ru>
2024-02-16 21:29:41 +0100

    title to lowercase

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = process.env.PORT || 3000;
 
-const products = [{title: 'jelly'}, {title: 'grind'}, {title: 'nook'}]
+const products = [{title: 'jelly'}, {title: 'grind'}, {title: 'Nook'}]
 
 app.get('/', (req: Request, res: Response) => {
   let startMessage = 'Hello Hania- oopsy, Hania! you did IT ))!'
@@ -12,7 +12,8 @@ app.get('/products', (req: Request, res: Response) => {
   res.send(products)
 })
 app.get('/products/:productTitle', (req: Request, res: Response) => {
-  let product = products.find(p => p.title === req.params.productTitle)
+  const productTitle = req.params.productTitle.toLowerCase()
+  let product = products.find(p => p.title.toLowerCase() === productTitle)
   res.send(product)
 })
 
