1738c32a76e642ea73d3b7a872bd0234a7b17769
NOVA <anna_tandem@mail.ru>
2024-02-16 23:39:42 +0100

    rrr

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,33 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 const products = [{title: 'jelly'}, {title: 'grind'}, {title: 'Nook'}]
+const addresses = [{id: 1, status: 'real'}, {id: 2, status: 'real'}, {id: 3, status: 'real'}]
 
 app.get('/', (req: Request, res: Response) => {
   let startMessage = 'Hello Hania- oopsy, Hania! you did IT ))!'
   res.send(startMessage)
 })
 app.get('/products', (req: Request, res: Response) => {
-  res.send(products)
+  if(req.query.title){
+    const stringFound = req.query.title.toString()
+    res.send(products.filter(p => p.title.indexOf(stringFound) >-1 ))
+  } else{
+    res.send(products)
+  }
 })
 app.get('/products/:productTitle', (req: Request, res: Response) => {
   const productTitle = req.params.productTitle.toLowerCase()
   let product = products.find(p => p.title.toLowerCase() === productTitle)
-  res.send(product)
+  if(product)
+  res.send(product) 
+else res.send(404)
+})
+
+app.get('/addresses/:id', (req: Request, res: Response) => { 
+  let address = addresses.find(p => p.id === +req.params.id)
+  if(address)
+  res.send(address) 
+else res.send(404)
 })
 
 app.listen(port,  () => {
